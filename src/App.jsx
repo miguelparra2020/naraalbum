043c0ebac96743ed3ea75da8745be0f9f3a557e6
@mpaez1 <mpaez@ikatechsolutions.com>
2024-08-25 20:05:08 -0500

    add-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import imagePortada from './assets/portada-nara.jpeg'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Albúm fotográfico</h1>
+      <MDBCard>
+      <MDBCardImage src={imagePortada} position='top' alt='...' 
+      width={"200"}
+      height={"320"}
+      />
+      <MDBCardBody>
+        <MDBCardTitle>Nara Páez Aguirre</MDBCardTitle>
+        <MDBCardText>
+          24/08/2024 9:18 pm
+        </MDBCardText>
+        <MDBBtn href='https://www.appsheet.com/start/e275bb84-455b-477a-bf38-6ed3b1e283e5' target='_blanck' color='danger'>Agregar fotografía <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera-fill" viewBox="0 0 16 16">
+  <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+  <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0"/>
+</svg></MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
     </>
   )
 }
