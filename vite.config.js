035307ff2218db6a53836e743379b639801df6c8
@mpaez1 <mpaez@ikatechsolutions.com>
2024-08-25 20:28:36 -0500

    add-changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nara P A - Admin',
+        short_name: 'App - Admin',
+        description: 'Administración de la aplicación Nara Páez Aguirre',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
