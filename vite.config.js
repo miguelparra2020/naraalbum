7306df333032bcaff568cafbe2e632d0dae87a20
@mpaez1 <mpaez@ikatechsolutions.com>
2024-08-25 21:42:35 -0500

    add-changes

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: '/icons/icon-192x192-2.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/icons/icon-512x512-2.png',
             sizes: '512x512',
             type: 'image/png'
           }
